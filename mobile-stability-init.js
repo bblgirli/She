2b7c72fbe426a60d2a5a82c1530c67d0742235af
kazeemb308-collab <kazeemb308@gmail.com>
2026-08-19 15:44:06 +0100

    Add mobile stability initializer

---
+++ mobile-stability-init.js
@@ -0,0 +1,7 @@
+/* M3ss3nger mobile stability init. */
+(function(){
+  var s=document.createElement('script');
+  s.src='./mobile-stability-loader.js?v=1';
+  s.defer=true;
+  document.head.appendChild(s);
+})();
