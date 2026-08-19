5ef4f04c78794390b331aad5f698b2fcc295d773
kazeemb308-collab <kazeemb308@gmail.com>
2026-08-19 16:54:02 +0100

    restore marker

--- app.js
+++ app.js
@@ -1,15 +1,1 @@
-import { firebaseConfig } from "./firebase-config.js";
-
-// ============================================
-// GLOBAL STATE & CONSTANTS
-// ============================================
-const STORAGE_KEY = "she_app_state";
-const CURRENT_USER_KEY = "she_current_user";
-
-let auth = null;
-let db = null;
-let firebaseApp = null;
-let firebaseInitialized = false;
-
-// Contacts and chats
-let userContacts = [];
+/* RESTORE FROM COMMIT 1291 */
