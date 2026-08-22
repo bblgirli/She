afcd5921c87706ccb98804be7dccdb4193fd1ea9
kazeemb308-collab <kazeemb308@gmail.com>
2026-08-22 10:52:51 +0100

    Activate session and presence UI coordination

--- app.js
+++ app.js
@@ -6,5 +6,6 @@ import "./app-fixes.js";
 import "./core/bootstrap.js";
 import "./core/session-coordinator.js";
 import "./core/presence-coordinator.js";
+import "./core/presence-ui.js";
 import "./core/service-registry.js";
 import "./feature-bootstrap.js";
